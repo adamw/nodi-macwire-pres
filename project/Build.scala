9826c808595f60c22fb4cfbe387c5e21fd5fe8c1
adamw <adam@warski.org>
2014-05-27 06:47:11 +0200

    Initial

---
+++ project/Build.scala
@@ -0,0 +1,39 @@
+import sbt._
+import Keys._
+
+object NodiMacwirePresBuild extends Build {
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization  := "com.softwaremill.nodi-macwire-pres",
+    version       := "1.0",
+    scalaVersion  := "2.11.1"
+  )
+
+  lazy val root: Project = Project(
+    "root",
+    file("."),
+    settings = buildSettings
+  ) aggregate(macros, core)
+
+  lazy val macros: Project = Project(
+    "macros",
+    file("macros"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _))
+  )
+
+  lazy val core: Project = Project(
+    "core",
+    file("core"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies ++= Seq(
+        // macwire
+        "com.softwaremill.macwire" %% "macros" % "0.6",
+        "com.softwaremill.macwire" %% "runtime" % "0.6",
+        // util
+        "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2",
+        "ch.qos.logback" % "logback-classic" % "1.1.2",
+        "org.scalatest" %% "scalatest" % "2.1.3" % "test"
+      ))
+  ) dependsOn (macros)
+}
