80b7330d13048f5e333f717127fe690082a139a5
adamw <adam@warski.org>
2014-06-12 11:43:46 +0200

    Formatting

--- play/demo/app/Global.scala
+++ play/demo/app/Global.scala
@@ -4,5 +4,6 @@ import play.api.GlobalSettings
 object Global extends GlobalSettings with Macwire {
   val instanceLookup = InstanceLookup(valsByClass(Application))
 
-  override def getControllerInstance[A](controllerClass: Class[A]) = instanceLookup.lookupSingleOrThrow(controllerClass)
+  override def getControllerInstance[A](controllerClass: Class[A]) =
+    instanceLookup.lookupSingleOrThrow(controllerClass)
 }
